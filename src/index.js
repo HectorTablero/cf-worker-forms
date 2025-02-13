244d93a215b2117efd63d7586cb675cf9af8476e
T4Bl3rUs <hellnight45games@gmail.com>
2025-02-13 16:46:58 +0100

    First commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,60 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export const retryPage = (redirect) => `
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Access Denied</title>
+</head>
+<body>
+  <h1>Invalid Email Domain</h1>
+  <p>Please use an email ending with @esnuam.org</p>
+  <a href="https://workers.tablerus.es/auth/logout?login=1&redirect=${encodeURIComponent(redirect)}">
+    Try different account
+  </a>
+</body>
+</html>`;
+
+export const loginPage = (redirect) => `
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+</head>
+<body>
+  <h1>Login Required</h1>
+  <a href="https://workers.tablerus.es/auth/login?redirect=${encodeURIComponent(redirect)}">
+    Login with Email
+  </a>
+</body>
+</html>`;
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+
+		if (url.pathname === '/') {
+			return new Response(null, { status: 200 });
+		} else if (url.pathname === '/login') {
+			return new Response(loginPage(url.searchParams.get('redirect') || 'https://forms.tablerus.es'), {
+				headers: { 'Content-Type': 'text/html' },
+			});
+		} else if (url.pathname === '/retry') {
+			return new Response(retryPage(url.searchParams.get('redirect') || 'https://forms.tablerus.es'), {
+				headers: { 'Content-Type': 'text/html' },
+			});
+		}
+
+		const cookies = request.headers.get('Cookie') || '';
+		const oauthId = cookies.match(/oauthId=([^;]*)/)?.[1];
+		if (!oauthId) {
+			return Response.redirect(`https://forms.tablerus.es/login?redirect=${encodeURIComponent(request.url)}`, 302);
+		}
+		let email = await env.FORMS.get(`auth:${oauthId}`, 'json');
+		if (!email) {
+			email = (await env.AUTH.verifyAuth(oauthId)).data.email;
+		}
+		if (!email.endsWith('@esnuam.org')) {
+			return Response.redirect(`https://forms.tablerus.es/retry?redirect=${encodeURIComponent(request.url)}`, 302);
+		}
+		return new Response('OK', { status: 200 });
 	},
 };
